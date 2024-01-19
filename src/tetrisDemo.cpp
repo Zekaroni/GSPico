43121021d2729628656e644c82914065d5a57e07
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2024-01-19 01:35:07 -0500

    updated tetris demo

--- src/tetrisDemo.cpp
+++ src/tetrisDemo.cpp
@@ -18,6 +18,7 @@ namespace Tetris
         Music::playTriangle(Music::E,2,Tetris::EIGHTH_NOTE,DAC::NULL_PIN);
         Music::playTriangle(Music::E,3,Tetris::EIGHTH_NOTE,DAC::NULL_PIN);
     };
+
     auto AA = []()
     {
         Music::playTriangle(Music::A,2,Tetris::EIGHTH_NOTE,DAC::NULL_PIN);
@@ -35,147 +36,230 @@ void tetris()
 {
     auto PWM_CHANNEL = []()
     {
-        // Bar 1
+        for (int i = 0; i < 2; i++) // Repeat
+        {
+            // Bar 1
+            Utility::pushData();
+            Music::playPWM(Music::E,5,Tetris::QUARTER_NOTE,PWM_PIN);
+            Music::playPWM(Music::B,4,Tetris::EIGHTH_NOTE,PWM_PIN);
+            Music::playPWM(Music::C,5,Tetris::EIGHTH_NOTE,PWM_PIN);
+            Music::playPWM(Music::D,5,Tetris::QUARTER_NOTE,PWM_PIN);
+            Music::playPWM(Music::C,5,Tetris::EIGHTH_NOTE,PWM_PIN);
+            Music::playPWM(Music::B,4,Tetris::EIGHTH_NOTE,PWM_PIN);
+            
+            // Bar 2
+            Utility::pushData();
+            Music::playPWM(Music::A,4,Tetris::QUARTER_NOTE-Tetris::THIRTYSECOND_NOTE,PWM_PIN);
+            Music::rest(Tetris::THIRTYSECOND_NOTE);
+            Music::playPWM(Music::A,4,Tetris::EIGHTH_NOTE,PWM_PIN);
+            Music::playPWM(Music::C,5,Tetris::EIGHTH_NOTE,PWM_PIN);
+            Music::playPWM(Music::E,5,Tetris::QUARTER_NOTE,PWM_PIN);
+            Music::playPWM(Music::D,5,Tetris::EIGHTH_NOTE,PWM_PIN);
+            Music::playPWM(Music::C,5,Tetris::EIGHTH_NOTE,PWM_PIN);
+
+            // Bar 3
+            Utility::pushData();
+            Music::playPWM(Music::B,4,Tetris::QUARTER_NOTE-Tetris::THIRTYSECOND_NOTE,PWM_PIN);
+            Music::rest(Tetris::THIRTYSECOND_NOTE);
+            Music::playPWM(Music::B,4,Tetris::EIGHTH_NOTE,PWM_PIN);
+            Music::playPWM(Music::C,5,Tetris::EIGHTH_NOTE,PWM_PIN);
+            Music::playPWM(Music::D,5,Tetris::QUARTER_NOTE,PWM_PIN);
+            Music::playPWM(Music::E,5,Tetris::QUARTER_NOTE,PWM_PIN);
+
+            // Bar 4
+            Utility::pushData();
+            Music::playPWM(Music::C,5,Tetris::QUARTER_NOTE,PWM_PIN);
+            Music::playPWM(Music::A,4,Tetris::QUARTER_NOTE-Tetris::THIRTYSECOND_NOTE,PWM_PIN);
+            Music::rest(Tetris::THIRTYSECOND_NOTE);
+            Music::playPWM(Music::A,4,Tetris::HALF_NOTE,PWM_PIN);
+
+            // Bar 5
+            Utility::pushData();
+            Music::rest(Tetris::EIGHTH_NOTE);
+            Music::playPWM(Music::D,5,Tetris::QUARTER_NOTE,PWM_PIN);
+            Music::playPWM(Music::F,5,Tetris::EIGHTH_NOTE,PWM_PIN);
+            Music::playPWM(Music::A,5,Tetris::QUARTER_NOTE,PWM_PIN);
+            Music::playPWM(Music::G,5,Tetris::EIGHTH_NOTE,PWM_PIN);
+            Music::playPWM(Music::F,5,Tetris::EIGHTH_NOTE,PWM_PIN);
+
+            // Bar 6
+            Utility::pushData();
+            Music::playPWM(Music::E,5,Tetris::QUARTER_NOTE+Tetris::EIGHTH_NOTE,PWM_PIN);
+            Music::playPWM(Music::C,5,Tetris::EIGHTH_NOTE,PWM_PIN);
+            Music::playPWM(Music::E,5,Tetris::QUARTER_NOTE,PWM_PIN);
+            Music::playPWM(Music::D,5,Tetris::EIGHTH_NOTE,PWM_PIN);
+            Music::playPWM(Music::C,5,Tetris::EIGHTH_NOTE,PWM_PIN);
+
+            // Bar 7
+            Utility::pushData();
+            Music::playPWM(Music::B,4,Tetris::QUARTER_NOTE-Tetris::THIRTYSECOND_NOTE,PWM_PIN);
+            Music::rest(Tetris::THIRTYSECOND_NOTE);
+            Music::playPWM(Music::B,4,Tetris::EIGHTH_NOTE,PWM_PIN);
+            Music::playPWM(Music::C,5,Tetris::EIGHTH_NOTE,PWM_PIN);
+            Music::playPWM(Music::D,5,Tetris::QUARTER_NOTE,PWM_PIN);
+            Music::playPWM(Music::E,5,Tetris::QUARTER_NOTE,PWM_PIN);
+
+            // Bar 8
+            Utility::pushData();
+            Music::playPWM(Music::C,5,Tetris::QUARTER_NOTE,PWM_PIN);
+            Music::playPWM(Music::A,4,Tetris::QUARTER_NOTE-Tetris::THIRTYSECOND_NOTE,PWM_PIN);
+            Music::rest(Tetris::THIRTYSECOND_NOTE);
+            Music::playPWM(Music::A,4,Tetris::HALF_NOTE,PWM_PIN);
+        };
+        // Bar 9
         Utility::pushData();
-        Music::playPWM(Music::E,5,Tetris::QUARTER_NOTE,PWM_PIN);
-        Music::playPWM(Music::B,4,Tetris::EIGHTH_NOTE,PWM_PIN);
-        Music::playPWM(Music::C,5,Tetris::EIGHTH_NOTE,PWM_PIN);
-        Music::playPWM(Music::D,5,Tetris::QUARTER_NOTE,PWM_PIN);
-        Music::playPWM(Music::C,5,Tetris::EIGHTH_NOTE,PWM_PIN);
-        Music::playPWM(Music::B,4,Tetris::EIGHTH_NOTE,PWM_PIN);
+        Music::playPWM(Music::E,4,Tetris::HALF_NOTE,PWM_PIN);
+        Music::playPWM(Music::C,4,Tetris::HALF_NOTE,PWM_PIN);
+
+        // Bar 10
+        Utility::pushData();
+        Music::playPWM(Music::D,4,Tetris::HALF_NOTE,PWM_PIN);
+        Music::playPWM(Music::B,3,Tetris::HALF_NOTE,PWM_PIN);
+
+        // Bar 11
+        Utility::pushData();
+        Music::playPWM(Music::C,4,Tetris::HALF_NOTE,PWM_PIN);
+        Music::playPWM(Music::A,3,Tetris::HALF_NOTE,PWM_PIN);
+
+        // Bar 12
+        Utility::pushData();
+        Music::playPWM(Music::GS,3,Tetris::HALF_NOTE,PWM_PIN);
+        Music::playPWM(Music::B,3,Tetris::HALF_NOTE,PWM_PIN);
+
+        // Bar 13
+        Utility::pushData();
+        Music::playPWM(Music::E,4,Tetris::HALF_NOTE,PWM_PIN);
+        Music::playPWM(Music::C,4,Tetris::HALF_NOTE,PWM_PIN);
+
+        // Bar 14
+        Utility::pushData();
+        Music::playPWM(Music::D,4,Tetris::HALF_NOTE,PWM_PIN);
+        Music::playPWM(Music::B,3,Tetris::HALF_NOTE,PWM_PIN);
         
-        // Bar 2
+        // Bar 15
         Utility::pushData();
-        Music::playPWM(Music::A,4,Tetris::QUARTER_NOTE-Tetris::THIRTYSECOND_NOTE,PWM_PIN);
-        Music::rest(Tetris::THIRTYSECOND_NOTE);
-        Music::playPWM(Music::A,4,Tetris::EIGHTH_NOTE,PWM_PIN);
-        Music::playPWM(Music::C,5,Tetris::EIGHTH_NOTE,PWM_PIN);
-        Music::playPWM(Music::E,5,Tetris::QUARTER_NOTE,PWM_PIN);
-        Music::playPWM(Music::D,5,Tetris::EIGHTH_NOTE,PWM_PIN);
-        Music::playPWM(Music::C,5,Tetris::EIGHTH_NOTE,PWM_PIN);
-
-        // Bar 3
-        Utility::pushData();
-        Music::playPWM(Music::B,4,Tetris::QUARTER_NOTE-Tetris::THIRTYSECOND_NOTE,PWM_PIN);
-        Music::rest(Tetris::THIRTYSECOND_NOTE);
-        Music::playPWM(Music::B,4,Tetris::EIGHTH_NOTE,PWM_PIN);
-        Music::playPWM(Music::C,5,Tetris::EIGHTH_NOTE,PWM_PIN);
-        Music::playPWM(Music::D,5,Tetris::QUARTER_NOTE,PWM_PIN);
-        Music::playPWM(Music::E,5,Tetris::QUARTER_NOTE,PWM_PIN);
-
-        // Bar 4
-        Utility::pushData();
-        Music::playPWM(Music::C,5,Tetris::QUARTER_NOTE,PWM_PIN);
-        Music::playPWM(Music::A,4,Tetris::QUARTER_NOTE-Tetris::THIRTYSECOND_NOTE,PWM_PIN);
-        Music::rest(Tetris::THIRTYSECOND_NOTE);
+        Music::playPWM(Music::C,4,Tetris::QUARTER_NOTE,PWM_PIN);
+        Music::playPWM(Music::E,4,Tetris::QUARTER_NOTE,PWM_PIN);
         Music::playPWM(Music::A,4,Tetris::HALF_NOTE,PWM_PIN);
 
-        // Bar 5
+        // Bar 16
         Utility::pushData();
-        Music::rest(Tetris::EIGHTH_NOTE);
-        Music::playPWM(Music::D,5,Tetris::QUARTER_NOTE,PWM_PIN);
-        Music::playPWM(Music::F,5,Tetris::EIGHTH_NOTE,PWM_PIN);
-        Music::playPWM(Music::A,5,Tetris::QUARTER_NOTE,PWM_PIN);
-        Music::playPWM(Music::G,5,Tetris::EIGHTH_NOTE,PWM_PIN);
-        Music::playPWM(Music::F,5,Tetris::EIGHTH_NOTE,PWM_PIN);
-
-        // Bar 6
-        Utility::pushData();
-        Music::playPWM(Music::E,5,Tetris::QUARTER_NOTE+Tetris::EIGHTH_NOTE,PWM_PIN);
-        Music::playPWM(Music::C,5,Tetris::EIGHTH_NOTE,PWM_PIN);
-        Music::playPWM(Music::E,5,Tetris::QUARTER_NOTE,PWM_PIN);
-        Music::playPWM(Music::D,5,Tetris::EIGHTH_NOTE,PWM_PIN);
-        Music::playPWM(Music::C,5,Tetris::EIGHTH_NOTE,PWM_PIN);
-
-        // Bar 7
-        Utility::pushData();
-        Music::playPWM(Music::B,4,Tetris::QUARTER_NOTE-Tetris::THIRTYSECOND_NOTE,PWM_PIN);
-        Music::rest(Tetris::THIRTYSECOND_NOTE);
-        Music::playPWM(Music::B,4,Tetris::EIGHTH_NOTE,PWM_PIN);
-        Music::playPWM(Music::C,5,Tetris::EIGHTH_NOTE,PWM_PIN);
-        Music::playPWM(Music::D,5,Tetris::QUARTER_NOTE,PWM_PIN);
-        Music::playPWM(Music::E,5,Tetris::QUARTER_NOTE,PWM_PIN);
-
-        // Bar 8
-        Utility::pushData();
-        Music::playPWM(Music::C,5,Tetris::QUARTER_NOTE,PWM_PIN);
-        Music::playPWM(Music::A,4,Tetris::QUARTER_NOTE-Tetris::THIRTYSECOND_NOTE,PWM_PIN);
-        Music::rest(Tetris::THIRTYSECOND_NOTE);
-        Music::playPWM(Music::A,4,Tetris::HALF_NOTE,PWM_PIN);
+        Music::playPWM(Music::GS,4,Tetris::WHOLE_NOTE,PWM_PIN);
     };
 
 
     auto bass = []()
     {
         while (1)
-        {
-            // Bar 1
-            Utility::waitForData();
-            Tetris::EE();
-            Tetris::EE();
-            Tetris::EE();
-            Tetris::EE();
+        {   
+            for (int i = 0; i < 2; i++) // Repeat
+            {
+                // Bar 1
+                Utility::waitForData();
+                Tetris::EE();
+                Tetris::EE();
+                Tetris::EE();
+                Tetris::EE();
 
-            // Bar 2
+                // Bar 2
+                Utility::waitForData();
+                Tetris::AA();
+                Tetris::AA();
+                Tetris::AA();
+                Tetris::AA();
+
+                // Bar 3
+                Utility::waitForData();
+                Tetris::GSGS();
+                Tetris::GSGS();
+                Tetris::EE();
+                Tetris::EE();
+
+                // Bar 4
+                Utility::waitForData();
+                Tetris::AA();
+                Tetris::AA();
+                Tetris::AA();
+                Music::playTriangle(Music::B,2,Tetris::EIGHTH_NOTE,DAC::NULL_PIN);
+                Music::playTriangle(Music::C,3,Tetris::EIGHTH_NOTE,DAC::NULL_PIN);
+
+                // Bar 5
+                Utility::waitForData();
+                Music::playTriangle(Music::D,3,Tetris::EIGHTH_NOTE-Tetris::THIRTYSECOND_NOTE,DAC::NULL_PIN);
+                Music::rest(Tetris::THIRTYSECOND_NOTE);
+                Music::playTriangle(Music::D,2,Tetris::QUARTER_NOTE-Tetris::THIRTYSECOND_NOTE,DAC::NULL_PIN);
+                Music::rest(Tetris::THIRTYSECOND_NOTE);
+                Music::playTriangle(Music::D,2,Tetris::QUARTER_NOTE-Tetris::THIRTYSECOND_NOTE,DAC::NULL_PIN);
+                Music::rest(Tetris::THIRTYSECOND_NOTE);
+                Music::playTriangle(Music::D,2,Tetris::THIRTYSECOND_NOTE,DAC::NULL_PIN);
+                Music::rest(Tetris::THIRTYSECOND_NOTE);
+                Music::playTriangle(Music::D,2,Tetris::SIXTEENTH_NOTE,DAC::NULL_PIN);
+                Music::playTriangle(Music::A,2,Tetris::EIGHTH_NOTE,DAC::NULL_PIN);
+                Music::playTriangle(Music::F,2,Tetris::EIGHTH_NOTE,DAC::NULL_PIN);
+
+                // Bar 6
+                Utility::waitForData();
+                Music::playTriangle(Music::C,2,Tetris::EIGHTH_NOTE,DAC::NULL_PIN);
+                Music::playTriangle(Music::C,3,Tetris::QUARTER_NOTE-Tetris::THIRTYSECOND_NOTE,DAC::NULL_PIN);
+                Music::rest(Tetris::THIRTYSECOND_NOTE);
+                Music::playTriangle(Music::C,3,Tetris::EIGHTH_NOTE,DAC::NULL_PIN);
+                Music::playTriangle(Music::C,2,Tetris::EIGHTH_NOTE,DAC::NULL_PIN);
+                Music::playTriangle(Music::G,2,Tetris::EIGHTH_NOTE-Tetris::THIRTYSECOND_NOTE,DAC::NULL_PIN);
+                Music::rest(Tetris::THIRTYSECOND_NOTE);
+                Music::playTriangle(Music::G,2,Tetris::EIGHTH_NOTE,DAC::NULL_PIN);
+                Music::playTriangle(Music::A,2,Tetris::EIGHTH_NOTE,DAC::NULL_PIN);
+
+                // Bar 7
+                Utility::waitForData();
+                Music::playTriangle(Music::B,2,Tetris::HALF_NOTE,DAC::NULL_PIN);
+                Music::playTriangle(Music::E,2,Tetris::HALF_NOTE,DAC::NULL_PIN);
+
+                // Bar 8
+                Utility::waitForData();
+                Music::playTriangle(Music::A,2,Tetris::QUARTER_NOTE-Tetris::THIRTYSECOND_NOTE,DAC::NULL_PIN);
+                Music::rest(Tetris::THIRTYSECOND_NOTE);
+                Music::playTriangle(Music::A,2,Tetris::QUARTER_NOTE-Tetris::THIRTYSECOND_NOTE,DAC::NULL_PIN);
+                Music::rest(Tetris::THIRTYSECOND_NOTE);
+                Music::playTriangle(Music::A,2,Tetris::HALF_NOTE,DAC::NULL_PIN);
+            };
+            // Bar 9
             Utility::waitForData();
             Tetris::AA();
             Tetris::AA();
-            Tetris::AA();
-            Tetris::AA();
 
-            // Bar 3
+            // Bar 10
             Utility::waitForData();
             Tetris::GSGS();
             Tetris::GSGS();
-            Tetris::EE();
-            Tetris::EE();
 
-            // Bar 4
+            // Bar 11
             Utility::waitForData();
             Tetris::AA();
             Tetris::AA();
+
+            // Bar 12
+            Utility::waitForData();
+            Tetris::GSGS();
+            Tetris::GSGS();
+
+            // Bar 13
+            Utility::waitForData();
             Tetris::AA();
-            Music::playTriangle(Music::B,2,Tetris::EIGHTH_NOTE,DAC::NULL_PIN);
-            Music::playTriangle(Music::C,3,Tetris::EIGHTH_NOTE,DAC::NULL_PIN);
-
-            // Bar 5
+            Tetris::AA();
+            
+            // Bar 14
             Utility::waitForData();
-            Music::playTriangle(Music::D,3,Tetris::EIGHTH_NOTE-Tetris::THIRTYSECOND_NOTE,DAC::NULL_PIN);
-            Music::rest(Tetris::THIRTYSECOND_NOTE);
-            Music::playTriangle(Music::D,2,Tetris::QUARTER_NOTE-Tetris::THIRTYSECOND_NOTE,DAC::NULL_PIN);
-            Music::rest(Tetris::THIRTYSECOND_NOTE);
-            Music::playTriangle(Music::D,2,Tetris::QUARTER_NOTE-Tetris::THIRTYSECOND_NOTE,DAC::NULL_PIN);
-            Music::rest(Tetris::THIRTYSECOND_NOTE);
-            Music::playTriangle(Music::D,2,Tetris::THIRTYSECOND_NOTE,DAC::NULL_PIN);
-            Music::rest(Tetris::THIRTYSECOND_NOTE);
-            Music::playTriangle(Music::D,2,Tetris::SIXTEENTH_NOTE,DAC::NULL_PIN);
-            Music::playTriangle(Music::A,2,Tetris::EIGHTH_NOTE,DAC::NULL_PIN);
-            Music::playTriangle(Music::F,2,Tetris::EIGHTH_NOTE,DAC::NULL_PIN);
+            Tetris::GSGS();
+            Tetris::GSGS();
 
-            // Bar 6
+            // Bar 15
             Utility::waitForData();
-            Music::playTriangle(Music::C,2,Tetris::EIGHTH_NOTE,DAC::NULL_PIN);
-            Music::playTriangle(Music::C,3,Tetris::QUARTER_NOTE-Tetris::THIRTYSECOND_NOTE,DAC::NULL_PIN);
-            Music::rest(Tetris::THIRTYSECOND_NOTE);
-            Music::playTriangle(Music::C,3,Tetris::EIGHTH_NOTE,DAC::NULL_PIN);
-            Music::playTriangle(Music::C,2,Tetris::EIGHTH_NOTE,DAC::NULL_PIN);
-            Music::playTriangle(Music::G,2,Tetris::EIGHTH_NOTE-Tetris::THIRTYSECOND_NOTE,DAC::NULL_PIN);
-            Music::rest(Tetris::THIRTYSECOND_NOTE);
-            Music::playTriangle(Music::G,2,Tetris::EIGHTH_NOTE,DAC::NULL_PIN);
-            Music::playTriangle(Music::A,2,Tetris::EIGHTH_NOTE,DAC::NULL_PIN);
+            Tetris::AA();
+            Tetris::AA();
 
-            // Bar 7
+            // Bar 16
             Utility::waitForData();
-            Music::playTriangle(Music::B,2,Tetris::HALF_NOTE,DAC::NULL_PIN);
-            Music::playTriangle(Music::E,2,Tetris::HALF_NOTE,DAC::NULL_PIN);
-
-            // Bar 8
-            Utility::waitForData();
-            Music::playTriangle(Music::A,2,Tetris::QUARTER_NOTE-Tetris::THIRTYSECOND_NOTE,DAC::NULL_PIN);
-            Music::rest(Tetris::THIRTYSECOND_NOTE);
-            Music::playTriangle(Music::A,2,Tetris::QUARTER_NOTE-Tetris::THIRTYSECOND_NOTE,DAC::NULL_PIN);
-            Music::rest(Tetris::THIRTYSECOND_NOTE);
-            Music::playTriangle(Music::A,2,Tetris::HALF_NOTE,DAC::NULL_PIN);
+            Music::playTriangle(Music::E,3,Tetris::WHOLE_NOTE,DAC::NULL_PIN);
         };
     };
 
